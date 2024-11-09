7c818d2922826e04c1af6d32829b4a329e8b68f8
Mohamed EL-Raghy <elraghy8@gmail.com>
2024-11-09 12:38:49 +0200

    first commit

---
+++ src/client/index.js
@@ -0,0 +1,8 @@
+// js files
+import { handleSubmit } from "./js/formHandler";
+import { checkForName } from "./js/nameChecker";
+
+alert("I EXIST");
+// console.log("CHANGE!!");
+
+// sass files
---
+++ src/server/index.js
@@ -0,0 +1,28 @@
+var path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+
+const cors = require("cors");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static("dist"));
+
+console.log(__dirname);
+
+// Variables for url and api key
+
+app.get("/", function (req, res) {
+  res.sendFile("dist/index.html");
+});
+
+// POST Route
+
+// Designates what port the app will listen to for incoming requests
+app.listen(8000, function () {
+  console.log("Example app listening on port 8000!");
+});
