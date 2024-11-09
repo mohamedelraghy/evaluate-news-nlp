8414d3bfecd63279b951e6c2851ddbf96085132b
Mohamed EL-Raghy <elraghy8@gmail.com>
2024-11-09 21:41:50 +0200

    define some missing moduels

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,9 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const WorkboxPlugin = require("workbox-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/client/index.js",
