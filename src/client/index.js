cfe26c2bd066310663079384f2c61dfa08211729
Mohamed EL-Raghy <elraghy8@gmail.com>
2024-11-09 21:00:13 +0200

    export handleSubmit and checkFor Name

--- src/client/index.js
+++ src/client/index.js
@@ -7,7 +7,9 @@ import "./styles/footer.scss";
 import "./styles/form.scss";
 import "./styles/header.scss";
 
-alert("I EXIST");
+// alert("I EXIST");
 // console.log("CHANGE!!");
 
 // sass files
+
+export { handleSubmit, checkForName };
